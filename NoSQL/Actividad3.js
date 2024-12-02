a1bb452dc84d978dddc001dbc4010e5163225834
Juan Gutiérrez <juan.gutierrez.ttc.840@unilibre.edu.co>
2024-12-01 20:57:08 -0500

    Actividad 3
    Mongo

---
+++ NoSQL/Actividad3.js
@@ -0,0 +1,12 @@
+// Crear la colección
+db.createCollection("Estudiantes")
+
+// Insertar documentos
+db.Estudiantes.insertMany([
+  { "nombre": "Juan", "edad": 20, "ciudad": "Bogotá" },
+  { "nombre": "Ana", "edad": 22, "ciudad": "Medellín" },
+  { "nombre": "Luis", "edad": 19, "ciudad": "Cali" }
+]);
+db.Estudiantes.find();
+db.Estudiantes.find({"ciudad": "Medellín"});
+db.Estudiantes.find({"edad": {$gt: 20}});
